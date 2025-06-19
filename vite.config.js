1cd91f414b4fd24ecce83da937b0e41f6d9d6b26
Marueee <d20221101806@siswa.upsi.edu.my>
2025-06-19 11:45:20 +0800

    try update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import {
     defineConfig
 } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -10,9 +9,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        cors: true,
-    },
 });
